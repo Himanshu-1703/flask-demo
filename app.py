470c76cf3a0eca815726977da134e828200ebe88
Himanshu <himanshu1703@gmail.com>
2024-01-13 18:32:34 +0530

    Created login.html

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return 'Hello World'
+    return render_template('login.html')
 
+@app.route('/perform-login',methods=['POST'])
+def perform_login():
+    response = request.form.to_dict()
+    return response
 
-
+# TODO 1. Make a Registration page.
+# TODO 2. Login and Registration page link
 
 if __name__ == "__main__":
     app.run(port= 8080,
